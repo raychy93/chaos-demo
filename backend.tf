a282e4719185925fa7412d094794c20da274f061
Ray Chy <raychy@Rays-MacBook-Pro.local>
2026-02-24 00:24:09 -0500

    backend update

--- backend.tf
+++ backend.tf
@@ -4,5 +4,6 @@ terraform {
      storage_account_name = "raytfstate12345"
      container_name       = "tfstate"
      key                  = "aks-sec-baseline.tfstate"
+     use_azuread_auth     = true
   }
 }
